9a78801e7780f83e234cccb95d171dec6123bf81
Aram <aram_sargsyan@edu.aua.am>
2017-09-09 22:05:29 +0400

    FIrst commit

---
+++ homework2.js
@@ -0,0 +1,12 @@
+	console.log(" Hope you like this!!! ");
+	console.log(" Have a nice day ");
+	function transform() {
+		var binary = prompt("Write a binary number and you will see what it is in decimal");
+		var binary2= prompt("Enter another one")
+	 document.write("Your 1st binary number in decimal is  ",   parseInt(binary, 2), ".");
+	 document.write("Your 2nd binary number in decimal is  ",   parseInt(binary2, 2), ".");
+	 var sum= parseInt(binary, 2) + parseInt(binary2, 2);
+	 var mult= parseInt(binary, 2) * parseInt(binary2, 2);
+	 document.write("The sum of these numbers is  ", sum, "     and the product is ", mult, ".");
+	 console.log(" Hope you like this!!! ");
+	 console.log(" Have a nice day ");}
